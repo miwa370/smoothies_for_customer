8fabcab1af0c7946e97237ac2a2373d51719c0f3
miwa370 <hashimoto.miwa.5063@members.co.jp>
2024-11-28 14:00:02 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,4 +51,5 @@ if ingredients_list:
 
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=TRUE)
+#st.text(smoothiefroot_response.json())
+st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=true)
